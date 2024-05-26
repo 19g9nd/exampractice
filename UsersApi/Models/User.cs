6595ce3980ff802ac90612b43b4a212e8cc36c22
19g9nd <ahadova2017@gmail.com>
2024-05-26 16:11:50 +0400

    Add Ocelot

--- UsersApi/Models/User.cs
+++ UsersApi/Models/User.cs
@@ -2,8 +2,8 @@ namespace UsersApi.Models
 {
     public class User
     {
-        
-       public int Id { get; set; }
+
+        public int Id { get; set; }
         public string Name { get; set; }
     }
 }
